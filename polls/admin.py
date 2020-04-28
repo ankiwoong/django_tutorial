512a1532ce4b27cb288ebfdd4b33fcfd8a5fd5a0
ankiwoong <ankiwoong@gmail.com>
2020-04-28 17:18:49 +0900

    Customizing the admin form

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Question, Choice
 
-# admin 페이지에 Question 출력
-admin.site.register(Question)
-admin.site.register(Choice)
+from .models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
