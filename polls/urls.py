52e6bd85a3414c975bd9091a392d635d642471b3
ankiwoong <ankiwoong@gmail.com>
2020-04-28 12:17:14 +0900

    Adding views

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
@@ -8,5 +7,12 @@ urlpatterns = [
     # view :  HttpRequest 객체를 첫번째 인수로 하고, 경로로 부터 '캡처된' 값을 키워드 인수로하여 특정한 view 함수를 호출
     # kwargs : 임의의 키워드 인수들은 목표한 view 에 사전형으로 전달
     # name : 템플릿을 포함한 Django 어디에서나 명확하게 참조
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,21 @@
 from django.http import HttpResponse
 
-
-'''
-1. 첫 번째 뷰 작성하기
-- URLconf : 뷰를 호출하려면 이와 연결된 URL 이 있어야 하는데 이를 사용하기 위해 사용
-https://docs.djangoproject.com/ko/3.0/intro/tutorial01/#write-your-first-view
-'''
+# URLconf : 뷰를 호출하려면 이와 연결된 URL 이 있어야 하는데 이를 사용하기 위해 사용
+# https://docs.djangoproject.com/ko/3.0/intro/tutorial01/#write-your-first-view
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
