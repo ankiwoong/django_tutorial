849f9e7985c2aa1a6ef454e2804adda245c9d664
ankiwoong <ankiwoong@gmail.com>
2020-04-28 17:30:07 +0900

    Customize the manager change list

--- polls/models.py
+++ polls/models.py
@@ -22,6 +22,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 # Choice : 선택의 텍스트와 투표 집계 / 각 선택은 질문과 관련
