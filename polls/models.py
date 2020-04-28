54d9cfc734e2a43da08515140ae2af65a27b47c2
ankiwoong <ankiwoong@gmail.com>
2020-04-28 16:53:56 +0900

    Bug fixes

--- polls/models.py
+++ polls/models.py
@@ -20,7 +20,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 # Choice : 선택의 텍스트와 투표 집계 / 각 선택은 질문과 관련
