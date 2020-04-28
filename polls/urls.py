0dfeb56b7eefce59473570500b53bd6275ec33d0
ankiwoong <ankiwoong@gmail.com>
2020-04-28 13:38:12 +0900

    Determining the namespace of a URL

--- polls/urls.py
+++ polls/urls.py
@@ -7,10 +7,9 @@ urlpatterns = [
     # view :  HttpRequest 객체를 첫번째 인수로 하고, 경로로 부터 '캡처된' 값을 키워드 인수로하여 특정한 view 함수를 호출
     # kwargs : 임의의 키워드 인수들은 목표한 view 에 사전형으로 전달
     # name : 템플릿을 포함한 Django 어디에서나 명확하게 참조
-    # ex: /polls/
     path('', views.index, name='index'),
-    # added the word 'specifics'
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
